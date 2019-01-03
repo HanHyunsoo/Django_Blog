42f374870cfb8269319c0b51d2b34f323e1164a7
HanHyunsoo <gustn4563@gmail.com>
2019-01-03 18:35:15 +0900

    Application extending

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+# 주석 친 코드는 Django 2.0 미만에서 쓰는 코드
+# from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    # url(r'^$', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'), # = /
+    path('post/<pk>/', views.post_detail, name='post_detail'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# 주석 친 코드는 Django 2.0 미만에서 쓰는 코드
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include, url
+from django.urls import path, include
+# from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
