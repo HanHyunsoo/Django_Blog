a2ede806920127df2d04356906763e9226e5e8f9
HanHyunsoo <gustn4563@gmail.com>
2019-01-06 02:37:59 +0900

    Add Form

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 # 주석 친 코드는 Django 2.0 미만에서 쓰는 코드
-# from django.conf.urls import url
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name='post_list'),
     path('', views.post_list, name='post_list'), # = /
-    path('post/<pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # url(r'^$', views.post_list, name='post_list'),
+    # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # url(r'^post/new/$', views.post_new, name='post_new'),
+    # url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
