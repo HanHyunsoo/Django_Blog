2544f36278514ad7bfb92d725bd71453f73c0ddd
HanHyunsoo <gustn4563@gmail.com>
2019-03-07 18:45:07 +0900

    Add image registration function

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import blog.views
 import portfolio.views
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
