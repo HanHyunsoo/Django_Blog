4955705e34886c80669f643974cde91e97fe7aa5
HanHyunsoo <gustn4563@gmail.com>
2019-03-07 19:10:00 +0900

    Add Accounts app

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import blog.views
-import portfolio.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
